947596edadc3e7533cfcd730fec9d3b2f9171e1b
kemilysantos <kemilysantos779@gmail.com>
2020-07-28 18:49:39 -0300

    Segundo dia

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
-import ButtonLink from '../Button'
-//import ButtonLink from '../components/ButtonLink';
+import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
 
-function Menu(){
-    return (
-        <nav className="Menu">
-            <a href="/">
-                <img className="Logo" src={Logo} alt="Logo"/>
-            </a>
-            
-            <ButtonLink className="ButtonLink" href="/">
-                Novo Vídeo
-            </ButtonLink>
-        </nav>
-    );
+function Menu() {
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="AluraFlix logo" />
+      </Link>
+
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
 }
+
 export default Menu;
